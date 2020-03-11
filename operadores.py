b074f112922ac42c154346af3f776f57f2a1f361
CristyanGil <30477234+CristyanGil@users.noreply.github.com>
2020-03-10 22:23:01 -0600

    Create operadores.py

---
+++ operadores.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 20 10:46:31 2019
+
+@author: Cristyan Gil
+"""
+
+###   Operadores  Aritméticos   ###
+suma = 20+32
+resta = 30-10
+mult = 3*6
+div = 80/4
+Pot = 2**4
+Comp = ((7-1)/3)**3+20
+
+
+segundos = 8000
+minutos = segundos/60
+horas = minutos/60
+print(horas)
+
+horas_enteras = int(horas)
+print(horas_enteras)
+
+###   Operadores  STR   ###
+a = "hola"
+b = "mundo"
+c = a + b * 3
+
+print(c)
+
+###   Operadores  relacion   ###
+cond1 = 3==3
+cond2 = suma != resta
+cond3 = len(b) > len(a)
+
+###   Operadores  lógicos   ###
+True or True
+False and True
