c3b93ac7690fdd50510a4a16d6a12d09fdcee5cf
CristyanGil <30477234+CristyanGil@users.noreply.github.com>
2020-03-10 22:27:51 -0600

    Create errores_comunes.py

---
+++ errores_comunes.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 22 12:05:35 2019
+
+@author: 80211447
+"""
+
+# %%
+print "Hola"
+
+# %% Importar un modulo que no existe
+import modulo_desconocido
+
+# %% Elemento fuera de rango
+D = [1, 2, 3]
+D[5]
+# %% Funcion no encontrada dentro de modulo
+from math import cubo
+
+# %% operacion no realizable
+'2' + 2 
+
+# %% Variable no definida
+edad_11
+
+# %% Division entre cero
+x = 100/0
